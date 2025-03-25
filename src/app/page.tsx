32b4fbb2178f188b3672d2d8c21767c8bcf6a4b9
0xdeathstalker <soumyamukherjee.work@gmail.com>
2025-03-25 18:33:47 +0530

    add: env validation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,9 @@
 import TokenSelector from "@/components/token-selector";
+import { env } from "@/env";
 
 export default function Home() {
+  console.log("[env] = ", { duneApiKey: env.NEXT_PUBLIC_DUNE_API_KEY });
+
   return (
     <div className="flex flex-col items-center justify-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
