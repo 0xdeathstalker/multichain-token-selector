2cf9a1d7e72f8f5a3f1225e9372ac800b90e92b6
0xdeathstalker <soumyamukherjee.work@gmail.com>
2025-03-27 23:42:25 +0530

    add: theme toggle & input

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,8 @@ import TokenSelector from "@/components/token-selector";
 import { useState } from "react";
 import { DuneProvider } from "@/provider";
 import { env } from "@/env";
+import { Input } from "@/components/ui/input";
+import { ThemeToggle } from "@/components/theme-toggle";
 
 export default function Home() {
   const [wallet, setWallet] = useState<string>(
@@ -12,7 +14,8 @@ export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <input
+        <ThemeToggle />
+        <Input
           type="text"
           value={wallet}
           onChange={(e) => setWallet(e.target.value)}
--- src/context/Provider.tsx
+++ src/context/Provider.tsx
@@ -3,14 +3,22 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import React from "react";
+import { ThemeProvider } from "./theme-provider";
 
 const queryClient = new QueryClient();
 
 export default function Provider({ children }: { children: React.ReactNode }) {
   return (
-    <QueryClientProvider client={queryClient}>
-      {children}
-      <ReactQueryDevtools initialIsOpen={false} />
-    </QueryClientProvider>
+    <ThemeProvider
+      attribute="class"
+      defaultTheme="system"
+      enableSystem
+      disableTransitionOnChange
+    >
+      <QueryClientProvider client={queryClient}>
+        {children}
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
+    </ThemeProvider>
   );
 }
