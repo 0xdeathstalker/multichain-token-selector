c27b61ae777b8c1c456bbc79640c86c084af9976
0xdeathstalker <soumyamukherjee.work@gmail.com>
2025-04-09 19:00:15 +0530

    modify: code structure

--- app/page.tsx
+++ app/page.tsx
@@ -1,63 +1,14 @@
-"use client";
-
-import ChainSelector from "@/components/chain-token-selector/chain-selector";
-import TokenSelector from "@/components/chain-token-selector/token-selector";
-import { ThemeToggle } from "@/components/theme-toggle";
-import { Input } from "@/components/ui/input";
-import { ChainIds } from "@/constants/chains";
-import { DuneProvider } from "@/context/dune-provider";
-import { env } from "@/env";
-import { Token } from "@/types";
-import { getChains } from "@/lib/utils";
-import { useState } from "react";
-import { Button } from "@/components/ui/button";
-
-const ADDRESS = "0xd8da6bf26964af9d7eed9e03e53415d37aa96045";
-
-export default function Home() {
-  const [wallet, setWallet] = useState<string>(ADDRESS);
-  const [token, setToken] = useState<Token>();
-  const [chainId, setChainId] = useState<ChainIds | undefined>();
-
-  const allowedChains = getChains();
-
+export default function Page() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <ThemeToggle />
-
-        <Input
-          type="text"
-          value={wallet}
-          onChange={(e) => setWallet(e.target.value)}
-          placeholder="Enter your wallet address"
-        />
-
-        <DuneProvider duneApiKey={env.NEXT_PUBLIC_DUNE_API_KEY ?? ""}>
-          <div className="flex flex-col gap-4">
-            {/* Chain Selector */}
-            <div className="flex items-center gap-2">
-              <ChainSelector
-                allowedChains={allowedChains}
-                selectedChainId={chainId}
-                setSelectedChainId={setChainId}
-              />
-              <Button onClick={() => setChainId(undefined)} disabled={!chainId}>
-                clear
-              </Button>
-            </div>
-
-            {/* Token Selector */}
-            <TokenSelector
-              token={token}
-              onTokenChange={setToken}
-              wallet={wallet}
-              chainId={chainId ? [chainId] : undefined}
-              excludeSpamTokens
-            />
-          </div>
-        </DuneProvider>
-      </main>
+    <div className="flex min-h-screen flex-col items-center justify-center bg-black text-white">
+      <div className="flex flex-col gap-6 px-4 text-center">
+        <h1 className="text-4xl font-bold tracking-tight sm:text-6xl md:text-7xl lg:text-8xl font-sans">
+          cracked ui components
+        </h1>
+        <h3 className="font-mono text-xl sm:text-3xl text-muted-foreground">
+          coming soon...
+        </h3>
+      </div>
     </div>
   );
 }
