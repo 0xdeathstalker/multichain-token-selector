41aa5ef20c13e8d4953e80ea5eef67d4c04e7680
0xdeathstalker <soumyamukherjee.work@gmail.com>
2025-04-09 19:21:14 +0530

    add: landing page (wip)

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,23 @@
+import { DotPattern } from "@/components/ui/dot-pattern";
+import { cn } from "@/lib/utils";
+
 export default function Page() {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center bg-black text-white">
+    <div className="relative flex h-screen w-full flex-col items-center justify-center overflow-hidden rounded-lg border bg-background">
       <div className="flex flex-col gap-6 px-4 text-center">
-        <h1 className="text-4xl font-bold tracking-tight sm:text-6xl md:text-7xl lg:text-8xl font-sans">
+        <h1 className="z-10 whitespace-pre-wrap text-center text-5xl font-medium tracking-tighter text-black dark:text-white font-sans">
           cracked ui components
         </h1>
-        <h3 className="font-mono text-xl sm:text-3xl text-muted-foreground">
+        <h3 className="font-mono text-xl sm:text-3xl font-medium">
           coming soon...
         </h3>
       </div>
+
+      <DotPattern
+        className={cn(
+          "[mask-image:radial-gradient(500px_circle_at_center,white,transparent)]"
+        )}
+      />
     </div>
   );
 }
