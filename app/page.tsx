880006d3a776cf9b77de1bf8f07ef1a2d94a9443
0xdeathstalker <soumyamukherjee.work@gmail.com>
2025-04-10 09:36:06 +0530

    add: background & gradient text

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,26 @@
-import { DotPattern } from "@/components/ui/dot-pattern";
-import { cn } from "@/lib/utils";
+import { SquaresBackground } from "@/components/ui/squares-background";
 
 export default function Page() {
   return (
-    <div className="relative flex h-screen w-full flex-col items-center justify-center overflow-hidden rounded-lg border bg-background">
-      <div className="flex flex-col gap-6 px-4 text-center">
-        <h1 className="z-10 whitespace-pre-wrap text-center text-5xl font-medium tracking-tighter text-black dark:text-white font-sans">
-          cracked ui components
+    <div className="w-screen h-screen">
+      <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-full flex flex-col gap-6 md:gap-10 px-4 text-center">
+        <h1 className="bg-gradient-to-br from-white via-[#d4d4d4] to-[#626262] bg-clip-text text-transparent inline-block text-center text-4xl sm:text-6xl md:text-7xl lg:text-8xl font-medium font-sans">
+          cracked components
         </h1>
-        <h3 className="font-mono text-xl sm:text-3xl font-medium">
+        <h3 className="bg-gradient-to-br from-white via-[#d4d4d4] to-[#626262] bg-clip-text text-transparent inline-block  font-mono text-xl sm:text-3xl md:text-4xl font-medium">
           coming soon...
         </h3>
       </div>
 
-      <DotPattern
-        className={cn(
-          "[mask-image:radial-gradient(500px_circle_at_center,white,transparent)]"
-        )}
-      />
+      <div className="absolute inset-0 -z-50 h-full">
+        <SquaresBackground
+          direction="diagonal"
+          speed={0.5}
+          squareSize={40}
+          borderColor="#333"
+          hoverFillColor="#222"
+        />
+      </div>
     </div>
   );
 }
