0976f21d5546c72e4e3cd5a04e9699ad2f32c4b4
0xdeathstalker <soumyamukherjee.work@gmail.com>
2025-04-10 17:35:17 +0530

    modify: shadow

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ export default function Page() {
   return (
     <div className="w-screen h-screen">
       <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-full flex flex-col gap-6 md:gap-10 px-4 text-center">
-        <h1 className="bg-gradient-to-br from-white via-[#d4d4d4] to-[#626262] bg-clip-text text-transparent inline-block text-center text-4xl sm:text-6xl md:text-7xl lg:text-8xl font-medium font-sans">
+        <h1 className="bg-gradient-to-br from-white via-[#d4d4d4] to-[#858585] bg-clip-text text-transparent inline-block text-center text-4xl sm:text-6xl md:text-7xl lg:text-8xl font-medium font-sans drop-shadow-lg">
           cracked components
         </h1>
         <TextShimmer
